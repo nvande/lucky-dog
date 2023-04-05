57fcfb03ad2dc18c5d5bd74999f43fb2293e0c59
nick <nick.vanderwoude@donefirst.com>
2023-04-04 22:49:19 -0500

    feat: browse page

---
+++ src/pages/BrowsePage.js
@@ -0,0 +1,15 @@
+import PageComponent from '../components/PageComponent';
+
+function BrowsePage() {
+	return (
+        <PageComponent>
+            <div className="mt-5">
+                <h1>
+                    Logged in!
+                </h1>
+            </div>
+        </PageComponent>
+	);
+}
+
+export default BrowsePage;
